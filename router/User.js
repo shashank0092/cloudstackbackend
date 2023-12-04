350b95e82c8c379366eb76edcb9e84a10896b9a5
SHASHANK <shashank7016204503@gmail.com>
2023-12-04 21:13:41 +0530

    Chnages Some Minor Issue

--- router/User.js
+++ router/User.js
@@ -6,6 +6,6 @@ const UserMiddleWare = require("../middleware/User")
 
 const UserControllers=require("../controllers/UserControllers")
 router.post("/register",UserControllers.Register)
-router.get("/login",UserControllers.Login)
+router.post("/login",UserControllers.Login)
 
 module.exports=router
